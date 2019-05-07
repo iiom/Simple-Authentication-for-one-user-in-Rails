a03a33b53e2f719f497d5958a062a6211c84e229
iiom <ioma766@gmail.com>
2019-05-07 20:37:53 +0300

    re

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :authors
   resources :books
-  resources :sessions, only: [:create, :new]
+  resources :sessions, only: [:create, :new, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
